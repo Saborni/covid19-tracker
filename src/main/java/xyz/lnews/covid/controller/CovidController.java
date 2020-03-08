7b22dbd7cf6ed276fee806f48144b2d56a731d72
Saborni <bhattacharyasab93@gmail.com>
2020-03-08 12:02:24 +0530

    added percent

--- src/main/java/xyz/lnews/covid/controller/CovidController.java
+++ src/main/java/xyz/lnews/covid/controller/CovidController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import xyz.lnews.covid.model.LocStats;
 import xyz.lnews.covid.service.CovidServiceImpl;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 
 @Controller
 public class CovidController {
@@ -18,12 +21,25 @@ public class CovidController {
 
     @GetMapping("/")
     public String homePage(Model model){
+
         List<LocStats> allStats = covidService.getAllStats();
         int totalReportedCases = allStats.stream().mapToInt(s->s.getLatestTotalCases()).sum();
         int totalLatestCases = allStats.stream().mapToInt(s->s.getDiffFromPrevDay()).sum();
+
+        int lastDayCount = totalReportedCases-totalLatestCases;
+
+        double percentChange = (1-1.0*(lastDayCount-totalLatestCases)/lastDayCount)*100;
+
+
+
+        System.out.println(totalLatestCases);
+        System.out.println(totalReportedCases);
+        System.out.println(percentChange);
+
         model.addAttribute("title","Covid-19 Tracker Tool");
         model.addAttribute("totalReportedCases",totalReportedCases);
         model.addAttribute("totalLatestCases",totalLatestCases);
+        model.addAttribute("percentChange",percentChange);
         model.addAttribute("locStats",allStats);
 
         return "homepage";
